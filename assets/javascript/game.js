24d220759148ea7a927f11f4c745c122c95ec1e4
Gabriel Waldron <Gabe.waldronn@gmail.com>
2019-03-05 16:24:17 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,10 +27,7 @@
     var objNumber = Math.floor(Math.random() * 40) + 1;
     $(".detail-interface").prepend('<p class="obj-number">Current Objective: ' + objNumber + '</p>');  
 
-    //Crystal Point Function
-    var crystalRandom = function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      };
+
 
     //MyScore Number
     var yourScore = 0;
@@ -39,40 +36,40 @@
     var wins = 0;
     var losses = 0;
 
-    
+    // Crystal Point Function
+    var crystalRandom = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
-
-
-    // Respond to clicks on the crystals
-    crystals.diamond.value = crystalRandom(1,16);
+    // Grabbing random crystal then adding value
+    crystals.diamond.value = crystalRandom(1,8);
     $('.diamond').click(function(){
         crystalPoints(crystals.ruby);
     });
-    crystals.ruby.value = crystalRandom(1,16);
+    crystals.ruby.value = crystalRandom(1,8);
     $('.ruby').click(function(){
         crystalPoints(crystals.ruby);
     });
-    crystals.emerald.value = crystalRandom(1,16);
+    crystals.emerald.value = crystalRandom(1,8);
     $('.emerald').click(function(){
         crystalPoints(crystals.ruby);
     });
-    crystals.sapphire.value = crystalRandom(1,16);
+    crystals.sapphire.value = crystalRandom(1,8);
     $('.sapphire').click(function(){
         crystalPoints(crystals.sapphire);
     });
 
     // Respond to clicks on the crystals
-   var crystalPoints = function(clickedCrystal) {
+    var crystalPoints = function(clickedCrystal) {
         yourScore += clickedCrystal.value;
         $(".user-score").text("Current Score " + yourScore);   
-        };
+    };
 
-
-    /// Loss & Win count
+    // Loss & Win count
     var scoreCount = function(){
         if (objNumb === yourScore){
         wins++;
         $(".wins").text(wins);
         }
-    }
+    };
   
